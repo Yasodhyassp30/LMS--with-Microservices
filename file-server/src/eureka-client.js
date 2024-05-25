4111a524f2194fefa73d788f29f65c21e66773e5
Malaka Rodrigo <shehanmalakarodrigo@gmail.com>
2024-05-25 15:14:19 +0530

    feat: register with eureka server

---
+++ file-server/src/eureka-client.js
@@ -0,0 +1,38 @@
+import { Eureka } from "eureka-js-client";
+import { APP_HOST, APP_PORT } from "./configs/configs.js";
+
+const eurekaClient = new Eureka({
+  instance: {
+    app: "nodejs-service",
+    instanceId: "nodejs-service-1",
+    hostName: APP_HOST,
+    ipAddr: "127.0.0.1",
+    statusPageUrl: `http://${APP_HOST}:${APP_PORT}/status`,
+    port: {
+      $: APP_PORT,
+      "@enabled": true,
+    },
+    vipAddress: "nodejs-service",
+    dataCenterInfo: {
+      "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+      name: "MyOwn",
+    },
+  },
+  eureka: {
+    host: "localhost",
+    port: 8761,
+    servicePath: "/eureka/apps/",
+  },
+});
+
+const startEurekaClient = () => {
+    eurekaClient.start((error) => {
+      if (error) {
+        console.error("Error starting Eureka client:", error);
+      } else {
+        console.log("Node.js service registered with Eureka");
+      }
+    });
+  };
+  
+  export { eurekaClient, startEurekaClient };
--- file-server/src/index.js
+++ file-server/src/index.js
@@ -1,20 +1,27 @@
-import compression from 'compression';
-import cors from 'cors';
-import express from 'express';
-import httpContext from 'express-http-context';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import { APP_HOST, APP_PORT, BASE_URL, IMMEDIATE_LOG_FORMAT, LOG_FORMAT, } from './configs/configs.js';
-import './env.js';
-import initializer from './initializer.js';
-import routes from './routes.js';
-import logger, { logStream } from './utils/logger.js';
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import httpContext from "express-http-context";
+import helmet from "helmet";
+import morgan from "morgan";
+import {
+  APP_HOST,
+  APP_PORT,
+  BASE_URL,
+  IMMEDIATE_LOG_FORMAT,
+  LOG_FORMAT,
+} from "./configs/configs.js";
+import "./env.js";
+import { startEurekaClient } from "./eureka-client.js";
+import initializer from "./initializer.js";
+import routes from "./routes.js";
+import logger, { logStream } from "./utils/logger.js";
 
 initializer().then(() => {
   const app = express();
 
-  app.set('port', APP_PORT);
-  app.set('host', APP_HOST);
+  app.set("port", APP_PORT);
+  app.set("host", APP_HOST);
   app.locals.title = process.env.APP_NAME;
   app.locals.version = process.env.APP_VERSION;
 
@@ -41,8 +48,14 @@ initializer().then(() => {
   // API Routes
   app.use(BASE_URL, routes);
 
-
-  app.listen(app.get('port'), app.get('host'), () => {
-    logger.info(`Server started at http://${app.get('host')}:${app.get('port')}${BASE_URL}`);
+  app.listen(app.get("port"), app.get("host"), () => {
+    // Start the Eureka client
+    startEurekaClient();
+    
+    logger.info(
+      `Server started at http://${app.get("host")}:${app.get(
+        "port"
+      )}${BASE_URL}`
+    );
   });
 });
