4f6b27eeff8b7e980c06a680ab8eb05b0e01ede1
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2024-05-26 21:26:01 +0530

    Add configs in discovery server

--- quiz-service/main.py
+++ quiz-service/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from src.routes.quiz_routes import router as quiz_router
 from src.models.database import Base, engine
+import py_eureka_client.eureka_client as eureka_client
+from contextlib import asynccontextmanager
 
-# Create the database tables
+
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+
+@asynccontextmanager
+async def init_eureka(app: FastAPI):
+    await eureka_client.init_async(
+        eureka_server="http://localhost:8761/eureka/",
+        app_name="quiz-service",
+        instance_port=8000,
+        instance_host="localhost",
+    )
+    yield
+
+
+app = FastAPI(lifespan=init_eureka)
 
 app.include_router(quiz_router)
